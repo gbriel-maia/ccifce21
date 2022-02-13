9d525bac6629c8805e2d0234527ec5c0761e16ad
gbriel-maia <79607767+gbriel-maia@users.noreply.github.com>
2022-02-13 19:05:17 -0300

    funcao determinante extendida

--- 2_sem/strDados/09-09_pendente/docMatrizAD_pendente.c
+++ 2_sem/strDados/09-09_pendente/docMatrizAD_pendente.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<locale.h>
 #include<stdlib.h>
-    #include<time.h>
-float ** alocMat(int lin, int col){
+#include<time.h>
+float **alocMat(int lin, int col){
     float **a;
     a = (float **) malloc (lin * sizeof(float));
     for (int i = 0; i < lin; i++){
@@ -12,42 +12,75 @@ return a;
 }
 void povoar(int lin, int col, float **mat){
     for (int i = 0; i < lin; i++){
-        for (int o = 0; o < col; o++){
-            scanf("%f", &mat[i][o]);
+        for (int j = 0; j < col; j++){
+            scanf("%f", &mat[i][j]);
         }
     }
 }
-    void povoarA(int lin, int col, float **mat){    //povoa com numreos aleatorios
-        srand(time(NULL));
-        for (int i = 0; i < lin; i++){
-            for (int o = 0; o < col; o++){
-                mat[i][o] = rand() % 100 /** 1.0*/;
-            }
+void povoarA(int lin, int col, float **mat){    //povoa com numeros aleatorios
+    srand(time(NULL));
+    for (int i = 0; i < lin; i++){
+        for (int j = 0; j < col; j++){
+            mat[i][j] = rand() % 100 /** 1.0*/;
         }
     }
+}
 void mostrar(int lin, int col, float **mat){
     for (int i = 0; i < lin; i++){
-        for (int o = 0; o < col; o++){
-            printf("%.1f\t", mat[i][o]);
+        for (int j = 0; j < col; j++){
+            printf("%.1f\t", mat[i][j]);
         }
         printf("\n");
     }
 }
-void determinante(int lin, int col, float **mat){
-    float det;
-    if (lin == 1 && col == lin){
-        det = mat[lin][col];
-    } else if (lin == 2 && col == lin){
-    } else if (lin == 3 && col == lin){
-    } else {
-    }
+float determinante(int lin, int col, float **mat){
+    if (lin == col){
+        float det = 0;
+        if (lin ==1){
+            det = mat[lin][lin];
+        }else{
+            int j = 1;
+            for (int i = 1; i <= col; i++){
+                det += mat[i][j] * compAlg(lin, col, i, j, mat);
+            }
+        }
 
-    printf("Determinante = %.2f ", det);
+return det;
+    }else{
+        printf("\nA matriz nao e quadrada");
+return 0;
+    }
+}
+
+float menComp(int lin, int col, int i, int j, float **mat){
+    float **nMat = alocMat(lin-1, col-1);
+    int nx = 0, ny = 0;
+    for (int x = 0; x < lin; x++){
+        if (x == i){
+            continue;
+        }
+        for (int y = 0; y < col; y++){
+            if (y == j){
+                continue;
+            }
+            nMat[nx][ny] = mat[x][y];
+            ny++;
+        }
+        ny = 0;
+        nx++;
+    }
+    float c = determinante(lin-1, col-1, nMat);
+    liberar(lin-1, col-1, nMat);
+return c;
+}
+float compAlg(int lin, int col, int i, int j, float **mat){
+    float a = (-1 ^ (i+j)) * menComp(lin, col, i, j, mat);
+return a;
 }
 void calcEscalar(int lin, int col, float **mat, float numE){
     for (int i = 0; i < lin; i++){
-        for (int o = 0; o < col; o++){
-            mat[i][o] *= numE;
+        for (int j = 0; j < col; j++){
+            mat[i][j] *= numE;
         }
     }
 }
@@ -60,12 +93,26 @@ void liberar(int lin, int col, float **mat){
 }
 int main(){
     setlocale(LC_ALL, "");
-    int lin = 3 , col = 3;
+    int lin = 0 , col = 0;
     float **mat, numE = 2;
-
-    //scanf("%d", &lin);
-    //scanf("%d", &col);
-    //scanf("%f", &numE); // numero escalar
+    
+    while (lin <= 0){
+        printf("Linha = ");
+        scanf("%d", &lin);
+        if (lin != 0){
+            break;
+        }
+        printf("\nLinha = %d Invalido\nTente novamente\n", lin);
+    }
+    while (col <= 0){
+        printf("Coluna = ");
+        scanf("%d", &col);
+        if (col != 0){
+            break;
+        }
+        printf("\nColuna = %d Invalido\nTente novamente\n", col);
+    }
+    scanf("%f", &numE); // numero escalar
 
     mat = alocMat(lin, col);
     //povoar(lin, col, mat);
@@ -73,8 +120,7 @@ int main(){
     mostrar(lin, col, mat);
     calcEscalar(lin, col, mat, numE);
     mostrar(lin, col, mat);
-    //determinante(lin, col, mat);
-    //mostrar(lin, col, mat);
+    printf("Determinante: %.2f\n", determinante(lin, col, mat));
 
     liberar(lin, col, mat);
 return 0;
